76556a8f5ea28819ab5f8daab86961f023c70e46
Junitalama <113072416+Junitalama@users.noreply.github.com>
2024-02-09 10:13:19 +0000

    form formatted

--- portfolio/src/components/Contact.jsx
+++ portfolio/src/components/Contact.jsx
@@ -14,11 +14,14 @@ const Contact = () => {
     <div
       name="contact"
       className="w-full h-screen bg-[#d8e9f3] flex justify-center items-center p-4"
-    > <form onSubmit = {handleSubmit}
-          action="https://getform.io/f/939f7ecf-2a91-4a0b-8b59-aa55ec3e2812"
-          method="POST"
-        >
-       <div className="pb-4">
+    >
+      {" "}
+      <form
+        onSubmit={handleSubmit}
+        action="https://getform.io/f/939f7ecf-2a91-4a0b-8b59-aa55ec3e2812"
+        method="POST"
+      >
+        <div className="pb-4">
           <p className="text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300">
             Contact
           </p>
@@ -27,17 +30,18 @@ const Contact = () => {
           </p>
         </div>
         <input
-          className="bg-[#ccd6f6] p-2"
+          className="bg-[#ccd6f6] p-2 mb-4"
           type="text"
           placeholder="Name"
           name="name"
         />
         <input
-          className="my-4 p-2 bg-[#ccd6f6]"
+          className="bg-[#ccd6f6] p-2 mb-4"
           type="email"
           placeholder="Email"
           name="email"
         />
+
         <textarea
           className="bg-[#ccd6f6] p-2"
           name="message"
@@ -47,14 +51,13 @@ const Contact = () => {
         <button className="text-white border-2 hover:bg-pink-600 hover:border-pink-600 font-bold px-4 py-3 my-8 mx-auto flex items-center">
           Submit
         </button>
-        </form>
-        {isFormSubmitted && (
-          <p className="text-green-600 font-bold text-center">
-            Message submitted successfully!
-          </p>
-        )}
-      </div>
-   
+      </form>
+      {isFormSubmitted && (
+        <p className="text-green-600 font-bold text-center">
+          Message submitted successfully!
+        </p>
+      )}
+    </div>
   );
 };
 export default Contact;
